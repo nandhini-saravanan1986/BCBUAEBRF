8fa8a1b10ed1330b5c6452f7ab5eecaa63c4b6ed
Nandhini <nandhini.j@bornfire.in>
2025-06-25 16:03:23 +0530

    Initial Commit

---
+++ src/main/java/com/bornfire/xbrl/entities/RT_FxRiskDataRepository.java
@@ -0,0 +1,27 @@
+package com.bornfire.xbrl.entities;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RT_FxRiskDataRepository extends JpaRepository<RT_Fxriskdata, Date> {
+	// Add custom queries if needed
+
+	@Query(value = "select * from BCBUAE_FX_RISK_DATA  ", nativeQuery = true)
+	List<RT_Fxriskdata> getlist();
+
+	@Query(value = "SELECT * FROM BCBUAE_FX_RISK_DATA ", nativeQuery = true)
+	List<RT_Fxriskdata> getfxriskdatalistdata();
+
+	@Query(value = "SELECT * FROM BCBUAE_FX_RISK_DATA ", nativeQuery = true)
+	List<Object[]> getfxriskdatalistdata1();
+	
+	
+	@Query(value = "SELECT * FROM BCBUAE_FX_RISK_DATA where SI_NO =?1 ", nativeQuery = true)
+	RT_Fxriskdata getParticularDataBySI_NO(String SI_NO);
+	
+}
