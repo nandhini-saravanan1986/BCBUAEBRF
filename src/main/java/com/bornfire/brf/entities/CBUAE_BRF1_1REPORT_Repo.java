1a026dc60dd560b2182c9c185409372f695b167e
Vinoth2106 <vinothkumar.ps@bornfire.in>
2025-07-06 17:54:58 +0530

    Update CBUAE_BRF1_1REPORT_Repo.java

--- src/main/java/com/bornfire/brf/entities/CBUAE_BRF1_1REPORT_Repo.java
+++ src/main/java/com/bornfire/brf/entities/CBUAE_BRF1_1REPORT_Repo.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CBUAE_BRF1_1REPORT_Repo extends JpaRepository<CBUAE_BRF1_1REPORT_ENTITY, Date> {
 
-	@Query(value = "select * from CBUAE_BRF1_1_SUMMARYTABLE  ", nativeQuery = true)
-	List<CBUAE_BRF1_1REPORT_ENTITY> getdatabydateList(Date rpt_code);
+	@Query(value = "select * from CBUAE_BRF1_1_SUMMARYTABLE where report_date=?1 ", nativeQuery = true)
+	List<CBUAE_BRF1_1REPORT_ENTITY> getdatabydateList(Date report_date);
 }
