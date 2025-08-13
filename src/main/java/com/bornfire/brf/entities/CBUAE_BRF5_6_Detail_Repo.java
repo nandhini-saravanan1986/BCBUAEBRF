1b5bc6bdfbd9a819946b00b711bae29c3042353a
Nishanthini <nishanthini.m@bornfire.co.in>
2025-08-13 11:58:12 +0530

    brf5_6 repochanges

--- src/main/java/com/bornfire/brf/entities/CBUAE_BRF5_6_Detail_Repo.java
+++ src/main/java/com/bornfire/brf/entities/CBUAE_BRF5_6_Detail_Repo.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CBUAE_BRF5_6_Detail_Repo extends JpaRepository<CBUAE_BRF5_6_Detail_Entity, String> {
+public interface CBUAE_BRF5_6_Detail_Repo extends JpaRepository<CBUAE_BRF5_6_Detail_Entity, Date> {
 
 	@Query(value = "select * from CBUAE_BRF5_6_DETAILTABLE where REPORT_DATE=?1", nativeQuery = true)
 	List<CBUAE_BRF5_6_Detail_Entity> getListbydate(Date reportdate);
